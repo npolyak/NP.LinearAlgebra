06fcaaee7c502eb28c271bbc68bc348ebdd56317
npolyak <nick.polyak@verizon.net>
2021-04-18 19:23:20 -0400

    added a project and a solution.

---
+++ MainTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
